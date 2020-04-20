63799e47b7e8c1b1d572715e0533eda4a80aa40c
Juan Cobian <cobianjuan19@gmail.com>
2020-04-20 20:34:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,28 +4,27 @@ class School
     @school_name = school_name
     @roster = {}
   end
-  
+
   def roster
     @roster
   end
-  
+
   def add_student(name, grade)
     @student_name = name
-    @student_grade = grade
+    @grade = grade
     if @roster.include?(grade) == false
       @roster[grade] = []
-    else
-      @roster[grade] << name
     end
+    @roster[grade] << name
   end
-  
+
   def grade(number)
     @roster[number]
   end
-  
+
   def sort
-    @roster.sort do |grade, name|
+    @roster.each do |grade, name|
       @roster[grade] = name.sort
-    end 
+    end
   end
 end
