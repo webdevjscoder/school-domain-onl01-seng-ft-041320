15cd4bfeef5e0ee1d04082c9f010da34ce515321
Juan Cobian <cobianjuan19@gmail.com>
2020-04-20 20:22:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,12 @@
 # code here!
 class School
+  def initialize(name)
+    @name = name
+  end
   
   def roster
     new = {}
   end 
-  
-  def initialize(name)
-    @name = name
-  end
   
   def add_student(student_name, student_grade)
     student_name.each do |student, grade|
