f298200e0cc0df2580d4696f203da113ec4036ce
Juan Cobian <cobianjuan19@gmail.com>
2020-04-20 20:31:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,8 +10,8 @@ class School
   end
   
   def add_student(student_name, student_grade)
-    @student_name = student_name
-    @student_grade = student_grade
+    @student_name = name
+    @student_grade = grade
     if @roster.include?(grade) == false
       @roster[grade] = []
     else
